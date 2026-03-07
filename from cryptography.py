1802af68e5c54e79ba9ec03b9a622f2fcbd10f9a
draga-arch <sashaoleksandraga@gmail.com>
2026-03-07 00:40:32 -0600

    Add files via upload

---
+++ from cryptography.py
@@ -0,0 +1,18 @@
+from cryptography.fernet import Fernet
+ 
+
+def write_key():
+    key = Fernet.generate_key()
+    with open("secret.key", "wb") as key_file:
+        key_file.write(key)
+
+def load_key():
+    return open("secret.key", "rb").read()
+
+def encrypt_file(filename, key):
+    f = Fernet(key)
+    with open(filename, "rb") as file:
+        file_data = file.read()
+    encrypted_data = f.encrypt(file_data)
+    with open(filename, "wb") as file:
+        file.write(encrypted_data)
